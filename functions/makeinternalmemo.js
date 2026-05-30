54e98bec4234da74772eda2f74bbfe5d1d0f9a48
collared1 <solarisgd1000@gmail.com>
2026-05-30 13:35:44 -0400

    Refactor onRequestPost to improve error handling

--- functions/makeinternalmemo.js
+++ functions/makeinternalmemo.js
@@ -1,35 +1,29 @@
 export async function onRequestPost(context) {
-  try {
-    const data = await context.request.json();
-    
-    const link = data.LINK;
+    try {
+        const data = await context.request.json();
 
-    console.log("Received document link:", link);
+        const link = data.LINK;
 
-    return new Response(
-      JSON.stringify({
-        success: true,
-        received: link
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json"
-        }
-      }
-    );
+        console.log("Received memo link:", link);
 
-  } catch (err) {
-    return new Response(
-      JSON.stringify({
-        success: false,
-        error: err.toString()
-      }),
-      {
-        headers: {
-          "Content-Type": "application/json"
-        },
-        status: 500
-      }
-    );
-  }
+        return new Response(JSON.stringify({
+            success: true,
+            LINK: link
+        }), {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+    } catch (err) {
+        return new Response(JSON.stringify({
+            success: false,
+            error: err.toString()
+        }), {
+            status: 500,
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+    }
 }
